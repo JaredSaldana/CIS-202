9adc36f12a6efc8ced13fb426694a3e36e4d4e57
JaredSaldana <47646956+JaredSaldana@users.noreply.github.com>
2019-02-19 11:23:51 -0800

    Update student.cpp

--- student.cpp
+++ student.cpp
@@ -2,29 +2,29 @@
 
 Student::Student(string students_name)
 {
-	name = students_name;
-	number_of_quizzes = 0;
-	total_score = 0;
+    this->name = students_name;
+    this->number_of_quizzes = 0;
+    this->total_score = 0;
 }
 
 string Student::get_name()
 {
-	return name;
+    return name;
 }
 
 void Student::add_quiz(int score)
 {
-	total_score += score;
-	number_of_quizzes++;
-	return total_score;
+    total_score += score;
+    number_of_quizzes++;
+    return total_score;
 }
 
 int Student::get_total_score()
 {
-	return total_score;
+    return total_score;
 }
 
 double Student::get_average_score()
 {
-	return ((double) total_score) / number_of_quizzes;
+    return ((double) total_score) / number_of_quizzes;
 }
