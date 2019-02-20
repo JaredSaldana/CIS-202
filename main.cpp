a0933d6fffb58ef245107b20b1533dad676cb5d7
JaredSaldana <47646956+JaredSaldana@users.noreply.github.com>
2019-02-20 12:58:41 -0800

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -1,8 +1,8 @@
 /*
     File: main.cpp
-    Description: 
-    Author:
-    Email:
+    Description: P5.9 & P5.10
+    Author: Jared Saldana
+    Email: jareds6070@student.vvc.edu
 */
 #include <iostream>
 #include <string>
