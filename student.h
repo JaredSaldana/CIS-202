0f9defa6ce0b4635b28a4db5bb30f1f600b05a61
JaredSaldana <47646956+JaredSaldana@users.noreply.github.com>
2019-02-20 12:51:16 -0800

    Update student.h

--- student.h
+++ student.h
@@ -1,30 +1,25 @@
+/*
+    File: student.h
+    Description: P5.9
+    Author: Jared Saldana
+    Email: jareds6070@student.vvc.edu
+*/
 #ifndef STUDENT_H
 #define STUDENT_H
-
+#include "grade.h"
 #include <string>
 
 class Student
 {
 public:
-	Student();
-	
-	Student(string students_name);
-	
-	string get_name();
-	
-	void add_quiz(int score);
-	
-	int get_total_score();
-
-	double get_average_score();
-	
+    Student();
+    Student(string students_name);
+    string get_name();
+    void add_grade(string grade);
+    double get_GPA();
 private:
-	string name;
-	
-	int total_score;
-	
-	int number_of_quizzes;
-	
+    string name;
+    Grade** all_Grades;
+    int number_of_grades;	
 };
-
 #endif
