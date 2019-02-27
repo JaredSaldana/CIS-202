5c332b1b4486d16fd4466adefa130c5a38f30e8b
JaredSaldana <47646956+JaredSaldana@users.noreply.github.com>
2019-02-26 16:18:54 -0800

    Create address.cpp

---
+++ address.cpp
@@ -0,0 +1,14 @@
+#include "address.h"
+#include <string>
+using namespace std;
+
+Address::Address(const string &street,)
+    :m_street(street), 
+{}
+
+void Address::set_street(tring &street)
+{
+    m_street = street;
+}
+
+string Address
